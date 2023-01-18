3a66214c06a43fe0e566d0a7571839a7424232dc
Mark Chris Levy <levy@codeandmore.pl>
2023-01-18 21:19:31 +0100

    Update MultimediaLibraryPlugin.swift
    Poprawione odpytywanie o pozwolenie na zapis

--- ios/Plugin/MultimediaLibraryPlugin.swift
+++ ios/Plugin/MultimediaLibraryPlugin.swift
@@ -7,19 +7,36 @@ public class MultimediaLibraryPlugin: CAPPlugin {
 
     @objc func saveImage(_ call: CAPPluginCall) {
 
-        let status = PHPhotoLibrary.authorizationStatus();
+        var status: PHAuthorizationStatus;
+        
+        if #available(iOS 14, *) {
+            status = PHPhotoLibrary.authorizationStatus(for: .addOnly)
+        } else {
+            status = PHPhotoLibrary.authorizationStatus()
+        };
 
         if (status == PHAuthorizationStatus.authorized) {
             doSaveImage(call);
 
         } else if (status == PHAuthorizationStatus.notDetermined) {
 
-            PHPhotoLibrary.requestAuthorization {status in
-                if (status == PHAuthorizationStatus.authorized) {
-                    self.doSaveImage(call);
-                } else {
-                    self.rejectNotAuthorized(call);
-                    return;
+            if #available(iOS 14, *) {
+                PHPhotoLibrary.requestAuthorization(for: .addOnly) {status in
+                    if (status == PHAuthorizationStatus.authorized) {
+                        self.doSaveImage(call);
+                    } else {
+                        self.rejectNotAuthorized(call);
+                        return;
+                    }
+                }
+            } else {
+                PHPhotoLibrary.requestAuthorization {status in
+                    if (status == PHAuthorizationStatus.authorized) {
+                        self.doSaveImage(call);
+                    } else {
+                        self.rejectNotAuthorized(call);
+                        return;
+                    }
                 }
             }
 
@@ -32,19 +49,36 @@ public class MultimediaLibraryPlugin: CAPPlugin {
 
     @objc func saveVideo(_ call: CAPPluginCall) {
 
-        let status = PHPhotoLibrary.authorizationStatus();
+        var status: PHAuthorizationStatus;
+        
+        if #available(iOS 14, *) {
+            status = PHPhotoLibrary.authorizationStatus(for: .addOnly)
+        } else {
+            status = PHPhotoLibrary.authorizationStatus()
+        };
 
         if (status == PHAuthorizationStatus.authorized) {
             doSaveVideo(call);
 
         } else if (status == PHAuthorizationStatus.notDetermined) {
 
-            PHPhotoLibrary.requestAuthorization {status in
-                if (status == PHAuthorizationStatus.authorized) {
-                    self.doSaveVideo(call);
-                } else {
-                    self.rejectNotAuthorized(call);
-                    return;
+            if #available(iOS 14, *) {
+                PHPhotoLibrary.requestAuthorization(for: .addOnly) {status in
+                    if (status == PHAuthorizationStatus.authorized) {
+                        self.doSaveVideo(call);
+                    } else {
+                        self.rejectNotAuthorized(call);
+                        return;
+                    }
+                }
+            } else {
+                PHPhotoLibrary.requestAuthorization {status in
+                    if (status == PHAuthorizationStatus.authorized) {
+                        self.doSaveVideo(call);
+                    } else {
+                        self.rejectNotAuthorized(call);
+                        return;
+                    }
                 }
             }
 
